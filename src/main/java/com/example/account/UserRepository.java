6a04d4dd0a29e8ead826642bfc5a9322f2fd3816
frs <frs2000@mail.ru>
2019-12-01 22:37:38 +0200

    Revert "wrong"
    This reverts commit 1dae57bf2e00461c52d8df2fb6a16d2135172eaa.

--- src/main/java/com/example/account/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.account;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
