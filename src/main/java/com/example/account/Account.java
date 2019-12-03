f3a3c018cc7776e57dd0d1f09f5b5ca53a0a78c3
frs <frs2000@mail.ru>
2019-12-04 00:29:46 +0200

    Updated Account

---
+++ src/main/java/com/example/account/Account.java
@@ -0,0 +1,14 @@
+package com.example.account;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Account {
+    private int id;
+    private String name;
+    private String surname;
+    private int account;
+}
