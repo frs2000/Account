19bd0299b2f1078e49166724148ee9e1f9046f2c
frs <frs2000@mail.ru>
2019-12-04 00:28:51 +0200

    Added AccountService

---
+++ src/main/java/com/example/account/AccountService.java
@@ -0,0 +1,15 @@
+package com.example.account;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public int create(Account account){
+        return 1;
+    }
+
+    public int getBalanceByID(int id){
+        return 1;
+    }
+}
