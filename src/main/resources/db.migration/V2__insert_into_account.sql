4130b0191c19a81e3046867dcc0b63621595c620
frs <frs2000@mail.ru>
2019-12-03 22:46:47 +0200

    Added V2__insert_into_account.sql

---
+++ src/main/resources/db.migration/V2__insert_into_account.sql
@@ -0,0 +1,8 @@
+INSERT INTO account (balance) VALUES (100);
+INSERT INTO account (balance) VALUES (200);
+INSERT INTO account (balance) VALUES (300);
+INSERT INTO account (balance) VALUES (0);
+
+
+
+
