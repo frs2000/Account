d859dcb19b13eeb7694dfa3f1d6a0160c6ee013e
frs <frs2000@mail.ru>
2019-12-04 00:31:08 +0200

    Added TransactionService

---
+++ src/main/java/com/example/transaction/TransactionService.java
@@ -0,0 +1,16 @@
+package com.example.transaction;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    public void putMoney(int toAccount, int amount){
+    }
+
+    public void withdrawMoney(int fromAccount, int amount){
+    }
+
+    public void transferMoney(int fromAccount, int amount, int toAccount){
+    }
+}
