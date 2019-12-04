cf638b59c2520a172bcc2b4b8025a87c586ce566
frs <frs2000@mail.ru>
2019-12-05 01:41:03 +0200

    Updated V1__create_account.sql

--- src/main/resources/db.migration/V1__create_account.sql
+++ src/main/resources/db.migration/V1__create_account.sql
@@ -1,6 +1,8 @@
 create table account
 (
     id      SERIAL NOT NULL,
+    name            VARCHAR(50)  NOT NULL,
+    surname     VARCHAR(300) NOT NULL,
     balance INT NOT NULL,
     PRIMARY KEY (id)
 );
