440c23b259bdb53b637838235ecba9823d0fa6b7
frs2000 <46113874+frs2000@users.noreply.github.com>
2020-02-17 21:21:58 +0200

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class Application {
-
+//2
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("Test log");
